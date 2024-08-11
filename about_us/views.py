788158861d3b9dc3b099ae55c2a1a0d635de70b0
Mostafa <mostafa.saneisn@gmail.com>
2024-08-11 22:55:29 +0330

    Create about_us view and template

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,11 @@
+from accounts.models import User
+from django.shortcuts import render
+
 def about_us(request):
     # View for about-us page
-    ...
+    users = User.objects.all()
+    user_names = [user.get_full_name() for user in users]
+    context = {
+        'user_names': user_names
+    }
+    return render(request, 'about_us.html', context)
